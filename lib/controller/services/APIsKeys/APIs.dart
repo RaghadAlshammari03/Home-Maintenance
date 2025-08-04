7b358b8ef4fff6f2fc3bc53b5d7dbdc73e1b3922
Raghad Alshammari <167905279+RaghadAlshammari03@users.noreply.github.com>
2025-08-04 06:04:52 +0300

    Remove tracked files now ignored by .gitignore

--- lib/controller/services/APIsKeys/APIs.dart
+++
@@ -1,9 +0,0 @@
-import 'package:baligny_technician/controller/services/APIsKeys/credential.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-class APIs {  
-  static String apiKey = dotenv.env['routesAPI']!;
-  static directionAPI(LatLng start, LatLng end) =>
-      'https://maps.googleapis.com/maps/api/directions/json?origin=${start.latitude},${start.longitude}&destination=${end.latitude},${end.longitude}&mode=driving&key=$apiKey';
-}
